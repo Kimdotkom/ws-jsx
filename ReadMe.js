5bf6565534cfc8cdc843f3b15b5a4ec9d17706d9
karim chebbi <karimchebbion@gmail.com>
2024-01-05 17:13:42 +0100

    first commit

---
+++ ReadMe.js
@@ -0,0 +1,16 @@
+/* ------------------------- JSX ------------------------- */
+
+// JSX stands for JavaScript Syntax extension
+// A mix of Html and JS code 
+// Describe how the UI should look like
+
+    let firstName = "Chris";
+    let lastName = "Aayari";
+
+
+
+        <div style={{textAlign: 'center', color: 'red'}}>
+
+                <p>Hello I'm {firstName}+ " " +{lastName}</p>
+
+        </div>
